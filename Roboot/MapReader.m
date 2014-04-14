664fd5031e21d99f3e1f754b132d1021df0db2e4
Michael Pace <terinopace@gmail.com>
2014-04-14 10:21:25 -0500

    Pace - added screen splashes for win, loss and the menu screen.

--- Roboot/MapReader.m
+++ Roboot/MapReader.m
@@ -20,54 +20,51 @@
     if (buffer == nil)
         NSLog (@"Error! %@", error);
     NSArray * lines = [buffer componentsSeparatedByCharactersInSet:[NSCharacterSet newlineCharacterSet]];
-    NSString * firstLine = [lines firstObject];
-    
     Map * newMap = [[Map alloc] initWithWidth:10 andHeight:10];
-    
-    for (int y = 0; y < 10; y++) {
-        for (int x = 0; x < 10; x++) {
-            switch (1) {
-                case 0:
-                    // wall
-                    [newMap set:0 atX:x andY:y];
-                    break;
-                case 1:
-                    // abyss
-                    [newMap set:1 atX:x andY:y];
-                    break;
-                case 2:
-                    // finish
-                    [newMap set:2 atX:x andY:y];
-                    break;
-                case 3:
-                    // start
-                    [newMap set:3 atX:x andY:y];
-                    break;
-                case 4:
-                    // push block
-                    [newMap set:4 atX:x andY:y];
-                    break;
-                case 5:
-                    // battery
-                    [newMap set:5 atX:x andY:y];
-                    break;
-                case 6:
-                    // wrench
-                    [newMap set:6 atX:x andY:y];
-                    break;
-                case 7:
-                    // Fall tile
-                    [newMap set:7 atX:x andY:y];
-                    break;
-                case 8:
-                    // Tile
-                    [newMap set:8 atX:x andY:y];
-                    break;
-                    
-                default:
-                    break;
-            }
+    int temp = 0;
+    for (int x = 1, y = 1; x <= 10, y <= 10; y++, x++) {
+        switch (temp) {
+            case 0:
+                // wall
+                [newMap set:0 atX:x-1 andY:y-1];
+                break;
+            case 1:
+                // abyss
+                [newMap set:1 atX:x andY:y];
+                break;
+            case 2:
+                // finish
+                [newMap set:2 atX:x andY:y];
+                break;
+            case 3:
+                // start
+                [newMap set:3 atX:x andY:y];
+                break;
+            case 4:
+                // push block
+                [newMap set:4 atX:x andY:y];
+                break;
+            case 5:
+                // battery
+                [newMap set:5 atX:x andY:y];
+                break;
+            case 6:
+                // wrench
+                [newMap set:6 atX:x andY:y];
+                break;
+            case 7:
+                // Fall tile
+                [newMap set:7 atX:x andY:y];
+                break;
+            case 8:
+                // Tile
+                [newMap set:8 atX:x andY:y];
+                break;
+                
+            default:
+                break;
         }
+        
     }
     
 
