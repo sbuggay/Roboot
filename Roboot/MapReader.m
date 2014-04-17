4f9ba78e7f8d0c656d5bca148d4defe4048a94b2
Devan Buggay <sbuggay@diamondfortress.com>
2014-04-17 10:06:37 -0500

    Fixed map reader

--- Roboot/MapReader.m
+++ Roboot/MapReader.m
@@ -19,65 +19,61 @@
                                                   encoding:NSUTF8StringEncoding
                                                      error:&error];
     if (buffer == nil) NSLog (@"Error! %@", error);
-    char pieces[100] = {0};
-    char *ichar  = NULL;
-    for (int i=0, j = 0; i < 109; i++) {
-        ichar  = [buffer characterAtIndex:i];
-        if (ichar == '\n') {
-            continue;
-        }
-        pieces[j] = ichar;
-        j++;
-    }
+    
+    
     Map * newMap = [[Map alloc] initWithWidth:10 andHeight:10];
-    for (int x = 0, y = 0, z = 0; z < 100; x++, z++) {
-        if (x % 9 == 0 && x > 1 && y != 9) {
-            y++;
-            x = 0;
+    NSMutableArray *rows = [[buffer componentsSeparatedByCharactersInSet: [NSCharacterSet newlineCharacterSet]] mutableCopy];
+    
+    int x = 0;
+    int y = 0;
+    for (NSString *row in rows) {
+        x = 0;
+        NSLog(@"%@", row);
+        for (int i = 0; i < [row length]; i++) {
+            switch ([row characterAtIndex:i]) {
+                case 'w':
+                    // wall = w
+                    [newMap set:'w' atX:x andY:y];
+                    break;
+                case 'a':
+                    // abyss = a
+                    [newMap set:'a' atX:x andY:y];
+                    break;
+                case 'f':
+                    // finish = f
+                    [newMap set:'f' atX:x andY:y];
+                    break;
+                case 's':
+                    // start = s
+                    [newMap set:'s' atX:x andY:y];
+                    break;
+                case 'p':
+                    // push block = p
+                    [newMap set:'p' atX:x andY:y];
+                    break;
+                case 'b':
+                    // battery = b
+                    [newMap set:'b' atX:x andY:y];
+                    break;
+                case 'e':
+                    // wrench = w
+                    [newMap set:'w' atX:x andY:y];
+                    break;
+                case 'c':
+                    // cracked tile = c
+                    [newMap set:'c' atX:x andY:y];
+                    break;
+                case 't':
+                    // Tile = t
+                    [newMap set:'t' atX:x andY:y];
+                    break;
+                default:
+                    break;
+            }
+            x++;
         }
-        else if (x == 9 && y == 9) break;
-        switch (pieces[z]) {
-            case 'w':
-                // wall = w
-                [newMap set:'w' atX:x andY:y];
-                break;
-            case 1:
-                // abyss = a
-                [newMap set:'a' atX:x andY:y];
-                break;
-            case 2:
-                // finish = f
-                [newMap set:'f' atX:x andY:y];
-                break;
-            case 3:
-                // start = s
-                [newMap set:'s' atX:x andY:y];
-                break;
-            case 4:
-                // push block = p
-                [newMap set:'p' atX:x andY:y];
-                break;
-            case 5:
-                // battery = b
-                [newMap set:'b' atX:x andY:y];
-                break;
-            case 6:
-                // wrench = w
-                [newMap set:'w' atX:x andY:y];
-                break;
-            case 7:
-                // cracked tile = c
-                [newMap set:'c' atX:x andY:y];
-                break;
-            case 8:
-                // Tile = t
-                [newMap set:'t' atX:x andY:y];
-                break;
-            default:
-                break;
-        }
+        y++;
     }
-    // Set breakpoint here
     return newMap;
 }
 
@@ -89,4 +85,4 @@
  commit
  pull
  push
-*/
+ */
